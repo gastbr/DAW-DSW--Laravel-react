294a4b2dff0f291c6c30d8bbe3953828e1c82d74
AlumG <AlumG@majada.es>
2025-01-24 21:51:45 +0100

    a30 pending

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import './bootstrap';
+import ReactDOM from 'react-dom/client';
+import Main from './components/Main';
+import '../css/app.css';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Main />
+);
