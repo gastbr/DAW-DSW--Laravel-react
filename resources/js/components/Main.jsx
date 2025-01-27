52b59405ed3fed368f8780acf857700ef9b26ea4
AlumG <AlumG@majada.es>
2025-01-27 17:59:01 +0100

    A30 A31 finished, A32 pending

--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -3,31 +3,10 @@ import axios from 'axios';
 import ProductList from './ProductList';
 
 const Main = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
-    useEffect(() => {
-        // Función para obtener los productos desde el endpoint
-        const fetchProducts = async () => {
-            try {
-                const response = await axios.get('/api/products');
-                setProducts(response.data.data); // Asumiendo que los datos están en response.data.data
-                setLoading(false);
-            } catch (err) {
-                setError(err.message);
-                setLoading(false);
-            }
-        };
-
-        fetchProducts();
-    }, []); // El array vacío como segundo argumento asegura que esto se ejecute solo una vez
-
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
 
     return (
-        <ProductList products={products} />
+        <ProductList />
     );
 };
 
--- resources/js/components/ProductList.jsx
+++ resources/js/components/ProductList.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-const ProductList = ({ products }) => {
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        // Función para obtener los productos desde el endpoint
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('/api/products');
+                setProducts(response.data.data); // Asumiendo que los datos están en response.data.data
+                setLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+
+        fetchProducts();
+    }, []); // El array vacío como segundo argumento asegura que esto se ejecute solo una vez
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
     return (
         <div>
             <h2>Product List</h2>
